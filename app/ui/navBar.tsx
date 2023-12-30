9c9181a87c637475a2904d3a052e3bab0fabf3ce
mikava7 <i.mikava365@gmail.com>
2023-12-30 22:32:45 +0100

    connected to database

--- app/ui/navBar.tsx
+++ app/ui/navBar.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import Link from "next/link";
+
 const links = [
-  { href: "/books", label: "books" },
-  { href: "/reviews", label: "reviews" },
-  { href: "/recomendations", label: "recomendations" },
-  { href: "/cabinet", label: "cabinet" },
+  { href: "/books", label: "Books" },
+  { href: "/reviews", label: "Reviews" },
+  { href: "/recommendations", label: "Recommendations" },
+  { href: "/cabinet", label: "Cabinet" },
 ];
+
 export default function NavBar() {
   return (
     <nav className="bg-base-300 py-4">
-      <div className="navbar px-8 max-w-6xl mx-auto">
+      <div className="navbar flex items-center justify-between px-8 max-w-6xl mx-auto">
         <Link href="/" className="btn btn-primary">
           Neighbor Reads
         </Link>
-        <ul className="menu menu-horizontal md:ml-8">
-          {links.map((link) => {
-            return (
-              <li key={link.href}>
-                <Link href={link.href} className="capitalize">
-                  {link.label}
-                </Link>
-              </li>
-            );
-          })}
+        <ul className="menu menu-horizontal flex space-x-4">
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href} className="capitalize">
+                {link.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
---
+++ scripts/seed.js
@@ -0,0 +1,45 @@
+const { db, sql } = require("@vercel/postgres");
+const { books } = require("../app/lib/books-data.js");
+
+async function seedBooks(client) {
+  console.log("Starting seedBooks function");
+
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-oosp"`;
+
+    // Create the "books" table if it doesn't exist
+    const createTable = await sql`
+      CREATE TABLE IF NOT EXISTS books (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        author VARCHAR(255) NOT NULL,
+        country VARCHAR(255) NOT NULL,
+        link VARCHAR(255) NOT NULL,
+        pages INTEGER NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        year INTEGER NOT NULL
+      )
+    `;
+    console.log(`Created "books" table`);
+
+    // Insert data into the "books" table
+    const insertedBooks = await Promise.all(
+      books.map(async (book) => {
+        return client.sql`
+          INSERT INTO books (id, author, country, link, pages, title, year)
+          VALUES (${book.id}, ${book.author}, ${book.country}, ${book.link}, ${book.pages}, ${book.title}, ${book.year})
+          ON CONFLICT (id) DO NOTHING
+        `;
+      })
+    );
+
+    console.log(`Seeded ${insertedBooks.length} books`);
+
+    return {
+      createTable,
+      books: insertedBooks,
+    };
+  } catch (error) {
+    console.error("Error seeding books:", error);
+    throw error;
+  }
+}
